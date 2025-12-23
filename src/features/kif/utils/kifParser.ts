60e6c71a42066b33be00d996cfd864002ccded84
Your Name <you@example.com>
2025-12-23 19:26:32 +0900

    reorg

--- src/features/kif/utils/kifParser.ts
+++ src/features/kif/utils/kifParser.ts
@@ -1,6 +1,6 @@
 // parseKif.ts
-import type { KifData, Board, Hand, Move } from "../types/kif";
-import { createBoard } from "../hooks/useKifPlayerOrig";
+import type { KifData, Board, Hand, Move } from "../types";
+import { createBoard } from "../types";
 import { parseMoveLine } from "./kifParseMoveLine";
 
 
