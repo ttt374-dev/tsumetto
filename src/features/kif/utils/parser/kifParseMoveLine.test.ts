9f21eace2c2886438f279162637bc2deb0bcebae
Your Name <you@example.com>
2025-12-25 11:29:49 +0900

    moveline parsereslt

--- src/features/kif/utils/parser/kifParseMoveLine.test.ts
+++ src/features/kif/utils/parser/kifParseMoveLine.test.ts
@@ -8,6 +8,8 @@ describe("parseMoveLineText", () => {
         const result = parseMoveLine("   3 ２五歩(26)        ( 0:00/00:00:00)");
 
         expect(result).toEqual({
+            ok: true,
+            value: {
             moveNumber: 3,
             moveText: "２五歩",
             piece: {
@@ -18,12 +20,15 @@ describe("parseMoveLineText", () => {
             drop: false,
             isBlack: true,
         }
+        }
         )
     })
 
     it("同", () => {
-        const result = parseMoveLine("   2 同　玉(13)        ( 0:00/00:00:00)", {file: 3, rank: 2});
+        const result = parseMoveLine("   2 同　玉(13)        ( 0:00/00:00:00)", { file: 3, rank: 2 });
         expect(result).toEqual({
+            ok: true, 
+            value: {
             moveNumber: 2,
             moveText: "同　玉",
             piece: {
@@ -33,26 +38,32 @@ describe("parseMoveLineText", () => {
             from: { file: 1, rank: 3 },
             drop: false,
             isBlack: false,
+        }
         })
 
     })
     it("成らず", () => {
         const result = parseMoveLine("   5 ２四銀不成(25)         ( 0:00/00:00:00)")
         expect(result).toEqual({
+            ok: true,
+            value: {
             moveNumber: 5,
             moveText: "２四銀不成",
             piece: {
                 key: "銀", isBlack: true,
             },
             position: { file: 2, rank: 4 },
-            from: { file: 2, rank: 5},
+            from: { file: 2, rank: 5 },
             drop: false,
             isBlack: true,
+        }
         })
     })
     it("打", () => {
         const result = parseMoveLine("   5 ２四金打           ( 0:00/00:00:00)")
         expect(result).toEqual({
+            ok: true, 
+            value: {
             moveNumber: 5,
             moveText: "２四金打",
             piece: {
@@ -62,31 +73,44 @@ describe("parseMoveLineText", () => {
             from: null,
             drop: true,
             isBlack: true,
+        }
         })
     })
     it("右", () => {
         const result = parseMoveLine("   5 ２四金右(23)           ( 0:00/00:00:00)")
         expect(result).toEqual({
+            ok: true, 
+            value: {
             moveNumber: 5,
             moveText: "２四金右",
             piece: {
                 key: "金", isBlack: true,
             },
             position: { file: 2, rank: 4 },
-            from: { file:2, rank: 3 },
+            from: { file: 2, rank: 3 },
             drop: false,
             isBlack: true,
+        }
         })
     })
     it("投了", () => {
         const result = parseMoveLine("115 投了 (00:00/00:00:00)")
         expect(result).toEqual({
-            type: "resign"
+            ok: true,
+            value: {
+                type: "resign"
+
+            }
         })
     })
     it("不正値", () => {
         const result = parseMoveLine("20 金３２(44)")
-        expect(result).toEqual(null)
+        expect(result).toEqual({
+            ok: false,
+            error: {
+                message: "invalid move: 金３２: NaN undefined"
+            }
+        })
     })
 })
 
--- src/features/kif/utils/parser/kifParseMoveLine.ts
+++ src/features/kif/utils/parser/kifParseMoveLine.ts
@@ -3,32 +3,32 @@
 import type { Move, HandNew, Position, KifEvent, HandPieceKey, PlayerType } from '../../types'
 import type { PieceTypeKey,  } from '../../types/pieceType';
 import { parsePosition, parseFromToPosition } from './kifParsePosition';
-
+import type { ParseResult } from './parseResult';
 
 // 1行の KIF を解析して Move オブジェクトへ
-export function parseMoveLine(line: string, prevPosition?: Position): KifEvent | null {
-  console.log("parse move line", line, prevPosition)
+export function parseMoveLine(line: string, prevPosition?: Position): ParseResult<KifEvent> {
+  //console.log("parse move line", line, prevPosition)
   //  例: "  5 １六歩(43)    ( 0:00/00:00:00)"
   const moveRegex = /^\s*(\d+)\s+([^\(]+?)(?:\((\d\d)\))?\s*\(/;
   const m = line.match(moveRegex);
-  if (!m) return null;
+  if (!m) return { ok: false, error: { message: "no match"}};
 
   const moveNumber = parseInt(m[1], 10);
   const isBlack = moveNumber % 2 == 1
   const moveText = m[2].trim();  // "１六歩"
   //const from = m[3] || null;     // "43" 等、無ければ null
   const fromResult = parseFromToPosition(m[3])
-  if (!fromResult.ok) return null
+  if (!fromResult.ok) return fromResult
   const from = fromResult.value
 
   if (moveText.startsWith("投了")){
     console.log("投了！")
-    return { type: "resign"}
+    return { ok: true, value: { type: "resign"}}
   }
   const positionResult = parsePosition(moveText, prevPosition)
   if (!positionResult.ok){
     //throw new Error("invalid position: 同だが prevTo がない")
-    return null; // TODO: resulttype
+    return positionResult
   }
   const position = positionResult.value
   let drop = false  
@@ -51,7 +51,7 @@ export function parseMoveLine(line: string, prevPosition?: Position): KifEvent |
   
   
   console.log("parse move", moveNumber, isBlack, moveText, from, position, piece)
-  return { moveNumber, isBlack, piece, moveText, from, position, drop };
+  return {ok: true, value: { moveNumber, isBlack, piece, moveText, from, position, drop }}
 }
 
 /*
